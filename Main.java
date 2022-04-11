b1a02dc0496926defd8da7f14d01a483856d7374
OliaZakharcuk <74414048+OliaZakharcuk@users.noreply.github.com>
2022-04-11 18:57:33 +0300

    EX_9

--- Main.java
+++ Main.java
@@ -1,25 +1,52 @@
 package com.company;
-import java.util.Collections;
+import java.io.*;
+import java.io.IOException;
 
 public class Main {
 
+    public static double div(double number1, double number2){
+        return number1 / number2;
+    }
+    public static int readNumber(int start, int end) throws IOException {
+        BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+        System.out.println("Enter number: ");
+
+        try {
+            Integer number = Integer.parseInt(br.readLine());
+            if((number >= start) && (number <= end)){
+                return number;
+            }else  {
+                throw new IOException("Number should be an integer type in range[ " + start + ", " + end + "]");
+            }
+        }catch (NumberFormatException e){
+            System.out.println("Number should be an integer type in range[ " + start + ", " + end + "]");
+        }
+
+        return 0;
+
+    }
+
     public static void main(String[] args) {
-
-        Employee emp1 = new SalariedEmployee("waiter", "Oleh", "AA55", 30);
-        Employee emp2 = new SalariedEmployee("dog walker", "Bogdan", "AA56", 20);
-        Employee emp3 = new ContractEmployee("dev", "Stepan", true, 50000);
-        Employee emp4 = new ContractEmployee("dev", "Petro", true, 70000);
-        System.out.println("Unsorted list: ");
-        for (Employee i: Employee.emp) {
-            i.calculatePay();
-            System.out.println(i.toString());
+        double number1 = 5.5;
+        double number2 = 1.5;
+        try {
+            double res = div(number1, number2);
+            System.out.println(res);
         }
-        Collections.sort(Employee.emp);
-        System.out.println("Sorted list: ");
-        for (Employee i: Employee.emp) {
-
-            System.out.println(i.toString());
+        catch (Exception e){
+            e.getStackTrace();
+        }
+        try {
+            Integer res2 = readNumber(1,10);
+            System.out.println(res2);
+        }catch (Exception e){
+            System.out.println(e);
         }
 
+
+
+
+
+	// write your code here
     }
 }
